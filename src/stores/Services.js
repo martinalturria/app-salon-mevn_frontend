8de7390800b5d11664ff59079138a76989b42a1d
Martin Alturria <martinalturria@hotmail.com>
2023-06-11 20:22:55 -0300

    add new Views, ServicesComponents, and finish Routes

---
+++ src/stores/Services.js
@@ -0,0 +1,20 @@
+import { defineStore } from "pinia";
+import { ref, onMounted } from "vue";
+import ServicesApi from "../api/ServicesApi";
+
+export const useServicesStore = defineStore("services", () => {
+    const services = ref([]);
+
+    onMounted(async () => {
+        try {
+            const { data } = await ServicesApi.all();
+            services.value = data;
+        } catch (error) {
+            console.log(error);
+        }
+    });
+
+    return {
+        services,
+    };
+});
